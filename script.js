6907d07da70248b5bdcbd64b38dbd8dc262f0a4f
Erick384 <erickwachuma@gmail.com>
2023-04-06 19:16:29 +0300

    Age-Calculator Commits

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-function appendValue(value) {
-  document.getElementById("result").value += value;
-}
-
 function calculateAge() {
-  let birthdate = new Date(document.querySelector(".birthdate").value);
+  //Get the date inputs
+  let day = document.querySelector("#day").value;
+  let month = document.querySelector("#month").value;
+  let year = document.querySelector("#year").value;
 
   //calculate the age
-  let ageInMilliseconds = Date.now() - birthdate.getTime();
-  let ageDate = new Date(ageInMilliseconds);
-  let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+  let today = new Date();
+  let birthdate = new Date(year, month - 1, day);
+  let age = today.getFullYear() - birthdate.getFullYear();
+  let m = today.getMonth() - birthdate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+      age--;
+  }
+  let d = today.getDate() - birthdate.getDate();
+  if (d < 0) {
+      m--;
+      d += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+  }
 
   // Display the age
-  document.querySelector(".output span").innerHTML = +age;
 
+  if (age > 0) {
+    document.querySelector(".yearOutput").innerHTML = + age;
 }
+if (m > 0) {
+    document.querySelector(".monthOutput").innerHTML = + m;
+}
+document.querySelector(".dayOutput").innerHTML = + d;
+
+//   let output = "Your age is ";
+//   if (age > 0) {
+//       output += age + " years, ";
+//   }
+//   if (m > 0) {
+//       output += m + " months, ";
+//   }
+//   output += d + " days.";
+//   document.querySelector(".ageOutput").innerHTML = output;
+}
+
+// document.querySelector(".yearOutput").innerHTML = + age;
+// document.querySelector(".monthOutput").innerHTML = + m;
+// document.querySelector(".dayOutput").innerHTML = + d;
